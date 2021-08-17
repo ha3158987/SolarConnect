3cf1f684ed71f62a10c1280d37fc8ffdc53b1d13
Hannah Park <ha3158987@gmail.com>
2021-08-17 22:39:21 +0900

    feat: useSort 커스텀훅 구현 (#17)

---
+++ src/utils/hooks/useSort.js
@@ -0,0 +1,31 @@
+const useSort = ({ numbers, direction }) => {
+  const arrangeAscendingOrder = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+          [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+      }
+    }
+    return arr;
+  };
+
+  const arrangeDescendingOrder = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] < arr[j]) {
+          [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+      }
+    }
+    return arr;
+  };
+
+  const arrangedNumberArr =
+    direction === "ascending"
+      ? arrangeAscendingOrder(numbers)
+      : arrangeDescendingOrder(numbers);
+  return arrangedNumberArr;
+};
+
+export default useSort;
